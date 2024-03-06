c63314c517eebe282edd982c9e8f6fc8f8582a71
pronoyf <113943150+pronoyf@users.noreply.github.com>
2024-03-06 13:44:52 -0500

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -5,15 +5,15 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@sugarcoatedtech.ca'
+$email_from = 'fuad.pronoy@gmail.com';
 
 $email_subject = "New Form Submission";
 $email_body = "User Name: $name.\n".
               "User Email:  $vistor_email.\n".
               "Subject: $subject. \n".
-              "User Message: $Vistor_email. \n";
+              "User Message: $message. \n";
 
-$to = 'fuad.pronoy@gmail.com';
+$to = 'pronoyfuad68@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
@@ -21,6 +21,6 @@ $headers  .= "Reply-To: $visitor_email  \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location: contact.html");
+header("Location: Contact.html");
 
 ?>
